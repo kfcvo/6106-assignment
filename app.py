d8402215e74fcdfe08127907aa02a5724d4efea2
zhirain <154818728+kfcvo@users.noreply.github.com>
2025-12-14 07:10:39 +0000

    finish

--- app.py
+++ app.py
@@ -31,6 +31,10 @@ def main():
 def paynow():
      return(render_template("paynow.html"))
 
+@app.route("/deposit",methods=["GET", "POST"])
+def deposit():
+     return(render_template("deposit.html"))
+
 @app.route("/userlog",methods=["GET", "POST"])
 def userlog():   
      conn = sqlite3.connect('user.db' )
